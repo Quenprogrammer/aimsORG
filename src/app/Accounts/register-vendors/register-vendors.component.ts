232c1c537c9e4b962e58d78a6d66ed47995289dd
sulaiman <jhnjahun@gmail.com>
2025-11-03 10:47:07 +0100

    Updated some pages

---
+++ src/app/Accounts/register-vendors/register-vendors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-vendors',
+  imports: [],
+  templateUrl: './register-vendors.component.html',
+  styleUrl: './register-vendors.component.css'
+})
+export class RegisterVendorsComponent {
+
+}
---
+++ src/app/App/vendors/vendors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendors',
+  imports: [],
+  templateUrl: './vendors.component.html',
+  styleUrl: './vendors.component.css'
+})
+export class VendorsComponent {
+
+}
